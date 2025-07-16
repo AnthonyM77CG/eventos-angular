af95355ae2e739a75abf8db1d0fd2b7ef2062421
Axel <asantanderalcarraz@gmail.com>
2025-07-16 01:17:06 -0500

    Fix locales

--- src/app/core/services/espacio-evento.service.ts
+++ src/app/core/services/espacio-evento.service.ts
@@ -1,31 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { EspacioEventoI } from '../models/espacio-evento';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EspacioEventoService {
-  private URL = 'http://localhost:8080/api/espacios_evento'
-  private http = inject(HttpClient)
+  private readonly URL = 'http://localhost:8080/api/espacios_evento';
+  private readonly http = inject(HttpClient);
 
-  getEspaciosEvento() {
-    return this.http.get<EspacioEventoI[]>(this.URL)
+  getEspaciosEvento(): Observable<EspacioEventoI[]> {
+    return this.http.get<EspacioEventoI[]>(this.URL);
   }
 
-  getEspacioEventoById(id: number) {
+  getEspacioEventoById(id: number): Observable<EspacioEventoI> {
     return this.http.get<EspacioEventoI>(`${this.URL}/${id}`);
   }
 
-  createEspacioEvento(espacio: EspacioEventoI) {
+  createEspacioEvento(espacio: EspacioEventoI): Observable<EspacioEventoI> {
     return this.http.post<EspacioEventoI>(this.URL, espacio);
   }
 
-  updateEspacioEvento(id: number, espacio: EspacioEventoI) {
+  updateEspacioEvento(id: number, espacio: EspacioEventoI): Observable<EspacioEventoI> {
     return this.http.put<EspacioEventoI>(`${this.URL}/actualizar/${id}`, espacio);
   }
 
-  deleteEspacioEvento(id: number){
-    return this.http.delete<EspacioEventoI>(`${this.URL}/eliminar/${id}`)
+  deleteEspacioEvento(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.URL}/eliminar/${id}`);
   }
 }
--- src/app/features/private/admin/pages/espacio-evento/espacio-evento.ts
+++ src/app/features/private/admin/pages/espacio-evento/espacio-evento.ts
@@ -1,42 +1,74 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { EspacioEventoService } from '../../../../../core/services/espacio-evento.service';
-import { Observable, shareReplay, tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { EspacioEventoI } from '../../../../../core/models/espacio-evento';
 import { AsyncPipe } from '@angular/common';
-
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-espacio-evento',
-  imports: [AsyncPipe],
+  standalone: true,
+  imports: [AsyncPipe, FormsModule],
   templateUrl: './espacio-evento.html',
-  styleUrl: './espacio-evento.scss'
+  styleUrls: ['./espacio-evento.scss']
 })
-export class EspacioEvento implements OnInit{
-  private espacioService = inject(EspacioEventoService)
-  protected espacios$!: Observable<EspacioEventoI[]>
-  protected espacio$!: Observable<EspacioEventoI> 
+export class EspacioEvento implements OnInit {
+  private readonly espacioService = inject(EspacioEventoService);
+
+  protected espacios$!: Observable<EspacioEventoI[]>;
+  protected showForm = false;
+  protected selectedEspacio: EspacioEventoI = {
+    nombre: '',
+    ubicacion: '',
+    aforoMaximo: 0,
+    descripcion: ''
+  };
+  protected isEditing = false;
 
   ngOnInit(): void {
-      this.getEspacioEvento()
+    this.getEspacioEvento();
   }
 
-  getEspacioEvento() {
-    this.espacios$ = this.espacioService.getEspaciosEvento()
+  getEspacioEvento(): void {
+    this.espacios$ = this.espacioService.getEspaciosEvento();
   }
 
-  getEspacioEventoById(id: number) {
-    this.espacio$ = this.espacioService.getEspacioEventoById(id)
+  openCreateForm(): void {
+    this.selectedEspacio = {
+      nombre: '',
+      ubicacion: '',
+      aforoMaximo: 0,
+      descripcion: ''
+    };
+    this.isEditing = false;
+    this.showForm = true;
   }
 
-  createEspacioEvento(espacio: EspacioEventoI) {
-    this.espacio$ = this.espacioService.createEspacioEvento(espacio)
+  openEditForm(espacio: EspacioEventoI): void {
+    this.selectedEspacio = { ...espacio };
+    this.isEditing = true;
+    this.showForm = true;
   }
 
-  updateEspacioEvento(id: number, espacio: EspacioEventoI) {
-    this.espacio$ = this.espacioService.updateEspacioEvento(id, espacio)
+  submitForm(): void {
+    const operation = this.isEditing
+      ? this.espacioService.updateEspacioEvento(this.selectedEspacio.id!, this.selectedEspacio)
+      : this.espacioService.createEspacioEvento(this.selectedEspacio);
+
+    operation.pipe(
+      tap(() => {
+        this.getEspacioEvento();
+        this.showForm = false;
+      })
+    ).subscribe();
   }
 
-  deleteEspacioEvento(id: number) {
-    this.espacio$ = this.espacioService.deleteEspacioEvento(id)
+  deleteEspacioEvento(id: number): void {
+    if (confirm('¿Estás seguro de eliminar este espacio?')) {
+      this.espacioService.deleteEspacioEvento(id).pipe(
+        tap(() => this.getEspacioEvento())
+      ).subscribe();
+    }
   }
 }
