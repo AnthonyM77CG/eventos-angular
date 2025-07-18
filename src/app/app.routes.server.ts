15fd02098651bec5d32eabc1a6c8c5d351184b32
Anthony Cotrina <anthony77CG@hotmail.com>
2025-07-18 18:59:35 -0500

    Agregar tipo de renderizado en rutas de editar en admin

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -4,5 +4,13 @@ export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'admin/locales/editar/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'admin/planes/editar/:id',
+    renderMode: RenderMode.Server
   }
 ];
